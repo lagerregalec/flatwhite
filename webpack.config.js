51cab3e16f4bc15f8bc3a12190a31e3bf94079e2
lagerregalec <allyomalley@hotmail.de>
2024-01-10 20:27:03 +0100

    removed livereload js from default.hbs old webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,11 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const LiveReloadPlugin = require('webpack-livereload-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-// Check if the current environment is development
-const isDevelopment = process.env.NODE_ENV === 'development';
-
-// Define an array to hold the list of plugins
-const plugins = [
-  new MiniCssExtractPlugin({
-    filename: 'style.[hash].css',
-  }),
-  new HtmlWebpackPlugin({
-    filename: 'default.hbs',
-    title: 'Custom template using Handlebars',
-    template: 'src/default.hbs',
-    minify: false
-  }),
-  new CopyPlugin({
-    patterns: [
-      { from: "./package.json", to: "." }
-    ],
-    options: {
-      concurrency: 100,
-    },
-  }),
-];
-
-// Add LiveReloadPlugin only in development
-if (isDevelopment) {
-  const LiveReloadPlugin = require('webpack-livereload-plugin');
-  plugins.push(new LiveReloadPlugin({}));
-}
-
 module.exports = {
+  //mode: 'production',
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -41,15 +13,80 @@ module.exports = {
     filename: '[name].js',
     clean: true,
   },
+  experiments: {
+    
+  },
+  //watch: true,
   watchOptions: {
     ignored: [path.resolve(__dirname, 'dist')],
   },
   module: {
     rules: [
-      // ... (unchanged)
+      {
+        test: /\.js$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'],
+      },
+      {
+        test: /\.(hbs)$/i,
+        include: path.resolve(__dirname, 'src'),
+        exclude: [
+          path.resolve(__dirname, 'src/default.hbs')
+        ],
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              context: path.resolve(__dirname, 'src'),
+              name: '[path][name].[ext]'
+            }
+          },
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|woff|woff2)$/i,
+        type: 'asset/resource',
+      }
     ],
   },
-  plugins: plugins,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.[hash].css',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'default.hbs',
+      title: 'Custom template using Handlebars',
+      template: 'src/default.hbs',
+      minify: false
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "./package.json", to: "." }
+      ],
+      options: {
+        concurrency: 100,
+      },
+    }),
+    new LiveReloadPlugin({})
+  ],
   devServer: {
     writeToDisk: true,
     contentBase: path.resolve(__dirname, 'dist'),
